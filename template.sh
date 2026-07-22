d73bea45eca70d9456b4f33ad1909217075deb8b
Shubham_IITG <Shubhamk.0991@op.iitg.ac.in>
2026-07-22 21:18:14 +0530

    Directory and Files added seccessfylly

---
+++ template.sh
@@ -0,0 +1,26 @@
+# Creating directory
+
+mkdir -p src
+mkdir -p research
+mkdir -p .github/workflows
+mkdir -p data
+mkdir -p static
+mkdir -p templates
+
+
+# creating files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch store_index.py
+touch Dockerfile
+touch requirements.txt
+touch research/trails.ipynb
+touch static/style.css
+touch templates/chat.html
+
+echo "Directory and files are created successfully."
